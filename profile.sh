74e44e3b5f0093783f76f34184b5636e025ad7b1
GawrAme <97426017+GawrAme@users.noreply.github.com>
2025-10-22 18:55:09 +0700

    Create profile.sh

---
+++ profile.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+clear
+neofetch
+echo -e " Selamat datang di layanan AutoScript LingVPN x 3X-ui"
+echo -e " Ketik \e[1;32mx-ui\e[0m untuk menampilkan menu interaktif"
+echo -e " Join Telegram Channel @https://t.me/LingVPN"
